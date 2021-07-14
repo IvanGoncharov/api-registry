7443b5c29c98866d6c7965749c2ca6af7b012ba0
Mike Ralphson <mike.ralphson@gmail.com>
2021-07-14 14:51:19 +0100

    fix: wait for tar pipe to emit finish event

--- backend.js
+++ backend.js
@@ -210,12 +210,15 @@ const driverFuncs = {
     const codeloadUrl = `https://codeload.github.com/${md.org}/${md.repo}/tar.gz/${md.branch}`;
     const res = await fetch(codeloadUrl, { cacheFolder: archiveCache });
     if (res.ok) {
-      const tarx = tar.x({ strip: 1, sync: true, C: `./metadata/${provider}.cache` });
-      await res.body.pipe(tarx).on('warn',function(code,message,data){
-        logger.warn('tar',message);
-      }).on('entry',function(entry){
-        logger.log('tar',entry.header.path);
+      const tarx = tar.x({ strip: 1, C: `./metadata/${provider}.cache` });
+      const stream = res.body.pipe(tarx);
+      stream.on('warn',function(code,message,data){
+        logger.warn('tar !',message);
       });
+      stream.on('entry',function(entry){
+        logger.log('tar x',entry.header.path);
+      });
+      await new Promise(fulfill => stream.on('finish', fulfill));
       const fileArr = await rf(`./metadata/${provider}.cache/`, { filter: md.glob, readContents: false, filenameFormat: rf.RELATIVE });
       let count = 0;
       for (let file of fileArr) {
